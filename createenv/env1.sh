08ab8cff58b696a7660394f98c7361bb2cca0d5c
liangjie.li(desktop) <liangjie.li@veritas.com>
2019-03-12 14:44:38 +0800

    update env1.sh

--- createenv/env1.sh
+++ createenv/env1.sh
@@ -3,35 +3,65 @@
 
 function before_install {
   echo "You must run this script with bash not sh!"
-  echo "make sure you have ran following commands"
-  echo "  apt install -y git"
-  echo "  mkdir ~/myprojects"
-  echo "  mkdir ~/downloads"
-  echo "  cd ~/myprojects; git clone https://github.com/ywllyht/mytools.git"
+  if [ ! -d ~/myproject ]; then
+      echo "mkdir ~/myproject"
+      mkdir ~/myproject
+  fi
+  if [ ! -d ~/myproject ]; then
+      echo "mkdir ~/Downloads"
+      mkdir ~/Downloads
+  fi
+  return 0
+  
 }
 
-function install_python {
+function install_anaconda {
   echo "install anaconda"
-  if [ -e ~/anaconda3 ]; then
+  if [ -d ~/anaconda3 ]; then
      echo "anaconda3 exists, skip"
-     return 1
+     return 0
   fi
   mkdir ~/Downloads
-  wget -nc -P ~/Downloads https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
-  bash ~/Downloads/Anaconda3-4.2.0-Linux-x86_64.sh
+  wget -nc -P ~/Downloads https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+  bash ~/Downloads/Anaconda3-5.2.0-Linux-x86_64.sh
 
   echo "upgrade pip"
   pip install --upgrade pip
   return 0
 }
 
+function install_git {
+  git --version
+  if [ $? -eq 0 ]; then 
+      echo "git is installed, skip"
+      return 0
+  fi
+  if [ -e /etc/redhat-release ]; then
+     sudo yum install -y git
+  else
+     sudo apt install -y git
+  fi
+  return 0
+}
+
+function download_mytools {
+  if [ -d ~/myproject/mytools ]; then
+     echo "mytools exists, skip"
+     return 0
+  fi
+  cd ~/myproject
+  git clone https://github.com/ywllyht/mytools.git
+
+}
+
 function install_cheat {
+  echo "you must run download_mytools first"
   cheat -v
   if [ $? -eq 0 ]; then
      echo "cheat is installed, skip"
-     return 1
+     return 0
   fi
-  pip install  cheat
+  pip install cheat
   echo "export CHEAT_EDITOR=vi" >> ~/.bashrc
   echo "export DEFAULT_CHEAT_DIR='~/myproject/mytools/cheat'" >> ~/.bashrc
   return 0
@@ -42,9 +72,9 @@ function install_thefuck {
   fuck -v
   if [ $? -eq 0 ]; then
       echo "thefuck is installed, skip"
-      return 1
+      return 0
   fi
-  pip install  thefuck
+  pip install thefuck
   echo "eval $(thefuck --alias)" >> ~/.bashrc
   return 0
  
@@ -54,65 +84,78 @@ function install_autojump {
   autojump -v 
   if [ $? -eq 0 ]; then
       echo "autojump is installed, skip"
-      return 1
+      return 0
   fi
-  sudo apt-get install -y autojump
-  echo "source /usr/share/autojump/autojump.sh" >> ~/.bashrc
+  if [ -e /etc/redhat-release ]; then
+     sudo yum install -y autojump
+     echo "source /etc/profile.d/autojump.sh" >> ~/.bashrc
+  else
+     sudo apt install -y autojump
+     echo "source /usr/share/autojump/autojump.sh" >> ~/.bashrc
+  fi
+  return 0
 
 }
+
 
 function install_tmux {
   tmux -V 
   if [ $? -eq 0 ]; then
       echo "tmux is installed, skip"
-      return 1
+      return 0
   fi
-  sudo apt-get install -y tmux
-  echo "set-window-option -g mouse on" >> ~/.tmux.conf
+  if [ -e /etc/redhat-release ]; then
+     sudo yum install -y tmux
+  else
+     sudo apt install -y tmux
+  fi
+  
+cat <<'EOF'>~/.tmux.conf
+
+set-window-option -g mouse on
+
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+
+# Other examples:
+# set -g @plugin 'github_username/plugin_name'
+# set -g @plugin 'git@github.com/user/plugin'
+# set -g @plugin 'git@bitbucket.com/user/plugin'
+
+set -g @plugin 'tmux-plugins/tmux-resurrect'
+# tmux-resurrect
+set -g @resurrect-save-bash-history 'on'
+set -g @resurrect-capture-pane-contents 'on'
+set -g @resurrect-strategy-vim 'session'
+# set -g @resurrect-save 'S'
+# set -g @resurrect-restore 'R'
+# to keep window's name after we reload from resurrect
+set-option -g allow-rename off
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '~/.tmux/plugins/tpm/tpm'
+EOF
+
+  
 
 }
 
-function other_configure {
-  echo "install macro for ky huice engine"
-  echo "eval \"$(_KY_COMPLETE=source ky)\"" >> ~/.bashrc
-}
-
-function install_tushare {
-  echo "install tushare"
-  pip install tushare
-}
-
-function install_talib {
-  mkdir ~/Downloads 2>/dev/null
-  cd ~/Downloads
-  wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-  tar xzvf ta-lib-0.4.0-src.tar.gz
-  cd ta-lib
-  ./configure
-  make
-  sudo make install
-  pip install ta-lib
-
-  echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> ~/.bashrc   # add this line in ~/.bashrc, fix could find "libta_lib.so.0" issue
-
-}
 
 command_sequence=(
    before_install 
-   install_python
+   install_git
+   install_anaconda
+   download_mytools
    install_cheat
    install_thefuck
    install_autojump
    install_tmux
-   install_tushare
-   install_talib
-   other_configure
-
 )
 
 for (( index=0; index<${#command_sequence[*]}; index++)); do
     echo ""
-    echo "==============="
+    
     echo "Executing ${index}th command '${command_sequence[$index]}'" 
     eval ${command_sequence[$index]}
     err=$?
@@ -122,5 +165,8 @@ for (( index=0; index<${#command_sequence[*]}; index++)); do
     else
       echo "Command '${command_sequence[$index]}' completed" 
     fi
+    echo "================================"
 done
+
+echo ""
 
--- createenv/test1.sh
+++ createenv/test1.sh
@@ -1,36 +1,60 @@
 #!/bin/bash
 # scripts to prepare dev env
 
-function install_python2 {
-  echo "make sure you have ran following commands"
-  echo "  apt install -y git"
-  echo "  mkdir ~/myprojects"
-  echo "  mkdir ~/downloads"
-  echo "  cd ~/myprojects; git clone https://github.com/ywllyht/mytools.git"
+function before_install {
+  echo "You must run this script with bash not sh!"
+  mkdir ~/myproject 2>/dev/null
+  mkdir ~/Downloads 2>/dev/null
+  return 0
+  
 }
 
-function install_python {
+function install_anaconda {
   echo "install anaconda"
   if [ -e ~/anaconda3 ]; then
      echo "anaconda3 exists, skip"
-     return 1
+     return 0
   fi
   mkdir ~/Downloads
-  wget -nc -P ~/Downloads https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
-  bash ~/Downloads/Anaconda3-4.2.0-Linux-x86_64.sh
+  wget -nc -P ~/Downloads https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+  bash ~/Downloads/Anaconda3-5.2.0-Linux-x86_64.sh
 
+  echo "upgrade pip"
+  pip install --upgrade pip
   return 0
+}
+
+function install_git {
+  git --version
+  if [ $? -eq 0 ]; then 
+      echo "git is installed, skip"
+      return 0
+  fi
+  if [ -e /etc/redhat-release ]; then
+     yum install -y git
+  fi
+     apt install -y git
+  fi
+  return 0
+}
+
+function download_mytools {
+  cd ~/myproject
+  git clone https://github.com/ywllyht/mytools.git
+
 }
 
 
 command_sequence=(
-   #before_install 
+   before_install 
+   install_git
    install_python
-   install_python2
-
+   download_mytools
 )
 
 for (( index=0; index<${#command_sequence[*]}; index++)); do
+    echo ""
+    echo "==============="
     echo "Executing ${index}th command '${command_sequence[$index]}'" 
     eval ${command_sequence[$index]}
     err=$?
@@ -38,7 +62,7 @@ for (( index=0; index<${#command_sequence[*]}; index++)); do
       echo "Command '${command_sequence[$index]}' failed with error $err " 
       #break
     else
-      log_msg "Command '${command_sequence[$index]}' completed" 
+      echo "Command '${command_sequence[$index]}' completed" 
     fi
 done
 
